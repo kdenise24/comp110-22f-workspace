c7cc18602a42c9cb9425c187313e26a6d3d5af3f
Kayla Duran <kdenise@unc.edu>
2022-09-19 15:49:44 -0400

    editied

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,8 +1,9 @@
-"""EX04 - `list` Utility Functions"""
+"""EX04 - `list` Utility Functions."""
 __author__ = "730566062"
 
-def all(list_ofints: (list[int]), single: int):
-    """given the list and the single int, returns a bool if they are equal"""
+
+def all(list_ofints: (list[int]), single: int) -> bool:
+    """Given the list and the single int, returns a bool if they are equal."""
     i: int = 0
     if len(list_ofints) == 0:
         return False
@@ -14,7 +15,7 @@ def all(list_ofints: (list[int]), single: int):
 
 
 def max(input: list[int]) -> int:
-    """given the lis, returns the max value/int"""
+    """Given the lis, returns the max value/int."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     else:
@@ -22,13 +23,13 @@ def max(input: list[int]) -> int:
         largest: int = input[0]
         while i < len(input):
             if largest < input[i]:
-                largest = input[1]
+                largest = input[i]
             i = i + 1
         return largest
 
 
-def is_equal(first_list: list[int], second_list: list[int]):
-    """takes both lists and checks in they are equal to each other"""
+def is_equal(first_list: list[int], second_list: list[int]) -> bool:
+    """Takes both lists and checks in they are equal to each other."""
     i: int = 0
     if len(first_list) != len(second_list):
         return False
